7f6b9c5f1d27fa5335836c6b76361cc919bcd216
HanyueZhang <zhang.hanyue@husky.neu.edu>
2016-09-23 16:31:31 -0400

    [387. First Unique Character in a String][Accepted]committed by LeetCode Extension

--- 387-First-Unique-Character-in-a-String/solution.java
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -3,21 +3,30 @@ public class Solution {
         if (s == null || s.length() == 0) {
             return -1;
         }
+//         int[] count = new int[256];
+// 		int[] index = new int[256];
+// 		for (int i = 0 ; i < s.length() ; i++) {
+// 			count[s.charAt(i) - 'A'] += 1;
+// 			index[s.charAt(i) - 'A'] = i;
+// 		}
+// 		int result = Integer.MAX_VALUE;
+// 		for (int i = 0 ; i < 256 ; i++) {
+// 			if (count[i] == 1 && index[i] < result) {
+// 				result = index[i];
+// 			}
+// 		}
+// 		if (result != Integer.MAX_VALUE) {
+// 			return result;
+// 		}
         int[] count = new int[256];
-		int[] index = new int[256];
-		for (int i = 0 ; i < s.length() ; i++) {
-			count[s.charAt(i) - 'A'] += 1;
-			index[s.charAt(i) - 'A'] = i;
-		}
-		int result = Integer.MAX_VALUE;
-		for (int i = 0 ; i < 256 ; i++) {
-			if (count[i] == 1 && index[i] < result) {
-				result = index[i];
-			}
-		}
-		if (result != Integer.MAX_VALUE) {
-			return result;
-		}
+        for (int i = 0 ; i < s.length() ; i++) {
+            count[s.charAt(i) - 'A'] += 1;
+        }
+        for (int i = 0 ; i < s.length() ; i++) {
+            if (count[s.charAt(i) - 'A'] == 1) {
+                return i;
+            }
+        }
 		return -1;
     }
 }
